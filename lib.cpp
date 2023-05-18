3fa98b570a4a76dc6f880a3286ef50f788e18193
MartaAponte <128090552+MartaAponte@users.noreply.github.com>
2023-05-18 22:13:51 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,33 +1,28 @@
-
-//
-// Created by marta on 18/05/2023.
-//
-
 #include "libreria.h"
-bool funzione1 (char a){
+bool riconoscimento(char a){
     int b ;
     b = int(a);
-
-    if((b<=90 && b>=65) || (b<=122 && b>=97)){
-
+    if((b>=65)and(b<=90)){
         return true  ;
+    }
+    else if((b>=97)and(b<=122)){
+        return true;
     }
     else {
         return false  ;
     }
 }
-char funzione2 (char a  ){
+char conversione(char a ){
     char b ;
     int c ;
     c  = int(a);
-    if ((c<=90) && (c>=65)){
+    if ((c>=65)and(c<=90)){
         b = char (c+32);
     }
-    else if ((c<=122) && (c>=97)){
+    else if ((c>=97)and(c<=122)){
 
         b = char (c - 32);
-
-
     }
     return b  ;
 }
+
