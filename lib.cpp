48e37b69ca4488483fe4d05f5e4cef53b4d956ad
MartaAponte <128090552+MartaAponte@users.noreply.github.com>
2023-05-18 22:03:12 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -6,7 +6,7 @@
 #include "libreria.h"
 bool funzione1 (char a){
     int b ;
-    b = (int)a;
+    b = int(a);
 
     if((b<=90 && b>=65) || (b<=122 && b>=97)){
 
@@ -17,17 +17,17 @@ bool funzione1 (char a){
     }
 }
 char funzione2 (char a  ){
-    char risultato;
-    int b ;
-    b = (int)a;
-    if (b<=90 && b>=65){
-        risultato = char (a+32);
+    char b ;
+    int c ;
+    c  = int(a);
+    if ((c<=90) && (c>=65)){
+        b = char (c+32);
     }
-    else if (b<=122 && b>=97){
+    else if ((c<=122) && (c>=97)){
 
-        risultato = char (a- 32);
+        b = char (c - 32);
 
 
     }
-    return risultato ;
+    return b  ;
 }
