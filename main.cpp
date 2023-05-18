f9ebf91b25b2dce5b30f07b9991c5418e5f541c0
MartaAponte <128090552+MartaAponte@users.noreply.github.com>
2023-05-18 17:20:48 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "libreria.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+
+    char a=0;
+    cin>> a;
+    if (funzione1(a)==true){
+        cout<<"vero"<<endl;
+        cout << funzione2(a);
+    }
+    else if (funzione1(a)==false){
+        cout <<"errore"<<endl;
+
+    }
+
+
+
+    return 0;
 }
+
+
